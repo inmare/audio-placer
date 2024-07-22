351f3d8d3280360147277d30c4fb7fbcb9f2b209
inmare <yatsumehole@naver.com>
2024-07-23 06:48:49 +0900

    project.js에 초기화 코드 할당

--- js/project.js
+++ js/project.js
@@ -1,4 +1,19 @@
+import { DEFAULT_AUDIO_INFO } from "./config";
+
 export default class Project {
-  static info = [];
+  static info = null;
   constructor() {}
+
+  static init(jsonData) {
+    Project.info = [];
+    for (let info of jsonData) {
+      // info object에 기본값 할당
+      for (let [key, value] of Object.entries(DEFAULT_AUDIO_INFO)) {
+        if (!info[key]) {
+          info[key] = value;
+        }
+      }
+      Project.info.push(info);
+    }
+  }
 }
--- js/script.js
+++ js/script.js
@@ -5,13 +5,4 @@ import Project from "./project";
 const res = await fetch("../playlist-info.json");
 const playlistInfo = await res.json();
 
-for (let info of playlistInfo) {
-  // info object에 기본값 할당
-  for (let [key, value] of Object.entries(DEFAULT_AUDIO_INFO)) {
-    if (!info[key]) {
-      info[key] = value;
-    }
-  }
-  Project.info.push(info);
-}
 initElement();
