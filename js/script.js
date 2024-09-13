b430f1e5ec198bd53abe0b636501c444f6fce4e2
inmare <yatsumehole@naver.com>
2024-09-13 14:06:31 +0900

    불필요한 초기화 코드 삭제

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-import { DEFAULT_AUDIO_INFO } from "./config";
 import initElement from "./elements/elements";
-import Project from "./project";
-
-const res = await fetch("../playlist-info.json");
-const playlistInfo = await res.json();
 
 initElement();
