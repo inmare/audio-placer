c298d0de364aad758fc532ca97810be877d8fc81
inmare <yatsumehole@naver.com>
2025-10-10 00:38:50 +0900

    리액트 기반으로 프로젝트 재 편성

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const ctx = canvasRef.current.getContext("2d");
+    if (!ctx) return;
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, 100, 100);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>Audio Placer</header>
+      <main>
+        <section>
+          <button>add shape</button>
+        </section>
+        <section>
+          <canvas
+            ref={canvasRef}
+            style={{ border: "1px solid black" }}
+          ></canvas>
+          <canvas></canvas>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
